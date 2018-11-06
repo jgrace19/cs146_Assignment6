aeab2b3936f8ab007983288853beadf3ebc2169f
JESS-PC\Jess <playin_bold@yahoo.com>
2018-11-06 08:10:08 -0800

    Elevator and flickering light

---
+++ Assignment6/Assets/FlickeringLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour {
+
+    Light light;
+    public float minWait;
+    public float maxWait;
+
+	// Use this for initialization
+	void Start () {
+        light = GetComponent<Light>();
+        StartCoroutine(Flashing());
+		
+	}
+	
+	IEnumerator Flashing()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWait, maxWait));
+            light.enabled = !light.enabled;
+        }
+    }
+		
+}
+
