8e771cc9f93bdc40b4b95716d57782daa540d5ac
cmguyman11 <cmguyman11@gmail.com>
2018-11-05 16:49:03 -0800

    Added ability to pick up and drop by pressing "k"

---
+++ Assignment6/Assets/Scripts/Pickup.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+
+    public GameObject item;
+    public GameObject tempParent;
+    public Transform guide;
+    bool carrying;
+    public float range = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+        item.GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (carrying == false)
+        {
+            if (Input.GetKeyDown(KeyCode.K) && ((guide.transform.position - transform.position).sqrMagnitude < range * range))
+            {
+                pickup();
+                carrying = true;
+            }
+        }
+        else if (carrying == true)
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+            {
+                drop();
+                carrying = false;
+            }
+        }
+    }
+    void pickup()
+    {
+        item.GetComponent<Rigidbody2D>().gravityScale = 0;
+        item.GetComponent<Rigidbody2D>().isKinematic = true;
+        item.transform.parent = tempParent.transform;
+        item.transform.localPosition = new Vector2(item.transform.localPosition.x, item.transform.localPosition.y + 1);
+        //item.transform.localPosition.Set(item.transform.localPosition.x, 3);
+            }
+    void drop()
+    {
+
+        item.GetComponent<Rigidbody2D>().gravityScale = 1;
+        item.GetComponent<Rigidbody2D>().isKinematic = false;
+        item.transform.parent = null;
+        item.transform.position.Set(guide.transform.position.x, guide.transform.position.y + 30, guide.transform.position.z);
+       // item.transform.position.Set(guide.transform.position.x, guide.transform.position.y + 30);
+
+    }
+}
+
